21f634893da6acbf5074c23238cfbc8ea075571a
Qamet Mammadli <qametmammadli7@gmail.com>
2021-06-03 15:51:13 +0400

    implement-generate-war-file

--- src/main/java/com/qamedev/restful/RestfulApplication.java
+++ src/main/java/com/qamedev/restful/RestfulApplication.java
@@ -2,11 +2,18 @@ package com.qamedev.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class RestfulApplication {
+public class RestfulApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(RestfulApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulApplication.class, args);
